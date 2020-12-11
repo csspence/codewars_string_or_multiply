1a132f5774f9ca3ca22714ac1cd723ed47ad851c
Crew Spence <csspence123@gmail.com>
2020-12-11 06:25:09 -0600

    Wrote working solution.

--- codewars_string_or_multiply.js
+++ codewars_string_or_multiply.js
@@ -4,6 +4,6 @@ Make a function that returns the value multiplied by 50 and increased by 6. If t
 Note: in C#, you'll always get the input as a string, so the above applies if the string isn't representing a double value.
 */
 
-function problem(x){
-  //your code here
+const problem = (x) => {
+  return (typeof x === 'number') === true ? x * 50 + 6 : 'Error';
 }
